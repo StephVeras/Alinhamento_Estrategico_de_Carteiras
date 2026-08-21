c3920442df27c9592988e0574a7cba485e01fcc9
StephVeras <ste.acc.veras@gmail.com>
2026-08-21 13:51:56 -0300

    Leitura dos dados de entrada e início do algoritmo

---
+++ src/CriaLista.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CriaLista {
+    
+        ArrayList<String> listaTeste = new ArrayList<>();
+        ArrayList<String> listaOficial = new ArrayList<>();
+
+    public ArrayList<String> encontraMaiorSequencia(ArrayList<String> lista1, ArrayList<String> lista2) {
+        
+        return lista1;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        int numero1;
+        int numero2;
+        ArrayList<String> lista1 = new ArrayList<>();
+        ArrayList<String> lista2 = new ArrayList<>();
+        Scanner stdin = new java.util.Scanner(System.in);
+
+        numero1 = stdin.nextInt();
+        numero2 = stdin.nextInt();
+        
+        for(int i =0; i< numero1;i++){
+            lista1.add(stdin.next());
+        }
+        for(int i =0; i< numero2;i++){
+            lista2.add(stdin.next());
+        }
+
+        
+        /*array oficial final e array teste
+        faz no array teste, pegando o elemento e comparando com os da outra lista
+        quando encontra elemento igual adiciona eno teste, e vai adicionando conforme vai achando.
+        Se for maior do que está no oficial substitui
+         */
+
+
+    }
 }
