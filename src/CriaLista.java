413a68ba39d0b1769bc43a758a7a3b67fe6c9077
StephVeras <ste.acc.veras@gmail.com>
2026-08-25 13:23:35 -0300

    Tentativa 1 de algoritmo completo, sem testes

--- src/CriaLista.java
+++ src/CriaLista.java
@@ -6,8 +6,36 @@ public class CriaLista {
         ArrayList<String> listaOficial = new ArrayList<>();
 
     public ArrayList<String> encontraMaiorSequencia(ArrayList<String> lista1, ArrayList<String> lista2) {
+        
+        for(int k=0; k<lista1.size(); k++){
+            int indice2 = 0;
 
-            for(int i =0; i< lista1.size();i++){
+            for(int i = k; i< lista1.size(); i++){
+
+                for(int j= indice2; j<lista2.size(); j++){
+
+                    if(lista1.get(i) == lista2.get(j)){
+                        indice2 = j;
+                        i++;
+                        listaTeste.add(lista2.get(j));
+                    }
+
+                }
+
+            }
+
+            if(listaTeste.size() >= listaOficial.size()){
+                listaOficial = listaTeste;
+            }
+            
+        }
+
+        return this.listaOficial;
+    }
+}
+
+/*
+for(int i =0; i< lista1.size();i++){
                 String teste = lista1.get(i);
                 
                 for(int j=0; j<lista2.size();j++){
@@ -20,7 +48,4 @@ public class CriaLista {
             if( this.listaTeste.size()> this.listaOficial.size()){
                 this.listaOficial = listaTeste;
             }
-
-        return this.listaOficial;
-    }
-}
+ */
